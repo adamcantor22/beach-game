f83075e5de725aaa79fd7cc2574df352d5999875
Unknown <adamcantor22@gmail.com>
2020-05-29 17:30:39 -0400

    Peep Creation
    Includes initial Move script and some prefab work

---
+++ Beach Game/Assets/Scripts/PeepController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PeepController : MonoBehaviour {
+
+	public float speed;
+
+	void Update() {
+		Move();
+	}
+
+	void Move() {
+		if(Input.GetAxis("Horizontal") != 0f || Input.GetAxis("Vertical") != 0f) {
+			Vector3 translation = new Vector3(Input.GetAxis("Horizontal"), 0f, Input.GetAxis("Vertical"));
+			translation *= speed * Time.deltaTime;
+			transform.position += translation;
+		}
+	}
+
+}
