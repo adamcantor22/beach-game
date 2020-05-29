fb3a38ea14be98326b64e42987cd69a356050416
Unknown <adamcantor22@gmail.com>
2020-05-29 18:10:19 -0400

    Added Rotation, fiddling with Rigidbody
    it's hard to remember what all this shit does after so long away from the 3D engine...

--- Beach Game/Assets/Scripts/PeepController.cs
+++ Beach Game/Assets/Scripts/PeepController.cs
@@ -4,18 +4,41 @@ using UnityEngine;
 
 public class PeepController : MonoBehaviour {
 
-	public float speed;
+	public float moveSpeed;
+	public float rotateSpeed;
+	public float jumpForce;
+	bool canJump = true;
+	Rigidbody rb;
+
+	void Start() {
+		rb = gameObject.GetComponent<Rigidbody>();
+	}
 
 	void Update() {
 		Move();
+		Rotate();
 	}
 
 	void Move() {
-		if(Input.GetAxis("Horizontal") != 0f || Input.GetAxis("Vertical") != 0f) {
-			Vector3 translation = new Vector3(Input.GetAxis("Horizontal"), 0f, Input.GetAxis("Vertical"));
-			translation *= speed * Time.deltaTime;
-			transform.position += translation;
+		if(Input.GetAxis("Vertical") != 0f) {
+			Vector3 translation = new Vector3(0f, 0f, Input.GetAxis("Vertical"));
+			translation *= moveSpeed * Time.deltaTime;
+			transform.Translate(translation);
 		}
+	}
+
+	void Rotate() {
+		if(Input.GetAxis("Horizontal") != 0f) {
+			Vector3 rotation = new Vector3(0f, Input.GetAxis("Horizontal"), 0f);
+			rotation *= rotateSpeed * Time.deltaTime;
+			transform.Rotate(rotation);
+		}
+	}
+
+	void Jump() {
+		// if(Input.GetAxis("Jump") != 0 && canJump) {
+		// 	rb.AddForce()
+		// }
 	}
 
 }
